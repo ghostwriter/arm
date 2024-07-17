c43340f10089d68e7da49db9e9e755eeae98d8a0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add TitleInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHub/PullRequest/TitleInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHub\PullRequest;
+
+use Ghostwriter\Arm\Interface\StringableInterface;
+
+interface TitleInterface extends StringableInterface
+{
+}
