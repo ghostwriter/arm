c2e7134a4d20d69f4317685dd4a237b5cab66d73
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:24 -0500

    Add PreReleaseInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Composer/Version/PreReleaseInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\Composer\Version;
+
+use Ghostwriter\Arm\Interface\StringableInterface;
+
+interface PreReleaseInterface extends StringableInterface
+{
+}
