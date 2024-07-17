1787a3f54d0fae850b610936a8a7c0c78c1cf260
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add IssueInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHub/IssueInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHub;
+
+interface IssueInterface
+{
+    public function create(string $issue): string;
+
+    public function delete(string $issue): string;
+
+    public function exists(string $issue): bool;
+
+    public function get(string $issue): string;
+
+    public function list(): array;
+
+    public function set(string $issue): string;
+}
