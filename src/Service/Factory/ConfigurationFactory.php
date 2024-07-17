7fc2c3a22de556396c81dabbb45df80256ec0466
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:24 -0500

    Add ConfigurationFactory.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Service/Factory/ConfigurationFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Service\Factory;
+
+use Ghostwriter\Arm\Configuration;
+use Ghostwriter\Arm\Interface\WorkspaceInterface;
+use Ghostwriter\Container\Interface\ContainerInterface;
+use Ghostwriter\Container\Interface\FactoryInterface;
+use Override;
+use RuntimeException;
+use Throwable;
+
+use function file_exists;
+use function sprintf;
+
+final readonly class ConfigurationFactory implements FactoryInterface
+{
+    /**
+     * @throws Throwable
+     */
+    #[Override]
+    public function __invoke(ContainerInterface $container): Configuration
+    {
+        $workspace = $container->get(WorkspaceInterface::class);
+
+        $configFile = $workspace->toString() . '/grm.php';
+
+        if (! file_exists($configFile)) {
+            throw new RuntimeException(sprintf('Configuration file "%s" does not exist.', $configFile));
+        }
+
+        return require $configFile;
+    }
+}
