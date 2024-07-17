2a8b82b994ca81103f73e03ded850fe396cea99b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:25 -0500

    Add TaskProcessor.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/TaskProcessor.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm;
+
+use Ghostwriter\Arm\Interface\TaskInterface;
+use Ghostwriter\Arm\Interface\TaskProcessorInterface;
+use Ghostwriter\Container\List\Providers;
+use Override;
+
+/** @see TaskProcessorTest */
+final readonly class TaskProcessor implements TaskProcessorInterface
+{
+    public function __construct(
+        private Providers $provider
+    ) {
+    }
+
+    #[Override]
+    public function process(TaskInterface $task): TaskInterface
+    {
+        //        foreach ($event->getTasks() as $task) {
+        //            $task();
+        //        }
+
+        return $task;
+    }
+
+    public function test(): bool
+    {
+        return true;
+    }
+}
