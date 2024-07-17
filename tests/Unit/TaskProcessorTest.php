357de44b6da033cc4bb3212194e333bb6796dfff
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:25 -0500

    Add TaskProcessorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/TaskProcessorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit;
+
+use Generator;
+use Ghostwriter\Arm\TaskProcessor;
+use Ghostwriter\Container\Container;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(TaskProcessor::class)]
+final class TaskProcessorTest extends TestCase
+{
+    #[DataProvider('dataProvider')]
+    public function test(bool $value): void
+    {
+        self::assertSame($value, $value);
+
+        self::assertTrue(Container::getInstance()->get(TaskProcessor::class)->test());
+    }
+
+    /**
+     * @return Generator<array{bool}>
+     */
+    public static function dataProvider(): Generator
+    {
+        yield from [
+            'true' => [true],
+            'false' => [false],
+        ];
+    }
+}
