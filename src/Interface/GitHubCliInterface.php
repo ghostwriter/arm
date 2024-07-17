eccf0eabe8d382ca0c7151690987912e0a128b95
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add GitHubCliInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHubCliInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface;
+
+interface GitHubCliInterface extends GitHubInterface
+{
+}
+
+// gh repo list {ORG|USER} --json 'id,nameWithOwner,defaultBranchRef,isFork,isTemplate,latestRelease,repositoryTopics,isArchived,visibility,url'
+
+//gh pr create --base {defaultBranchRef} --head origin:feature-branch --title 'Update dependencies' --body 'This patch updates dependencies' --label 'Automated'
