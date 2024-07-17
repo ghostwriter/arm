9bf732f3959062c203286345e7b2dd11fe217a59
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add TagInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHub/TagInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHub;
+
+interface TagInterface
+{
+    public function create(string $tag): string;
+
+    public function delete(string $tag): string;
+
+    public function exists(string $tag): bool;
+
+    public function get(string $tag): string;
+
+    public function list(): array;
+
+    public function set(string $tag): string;
+}
