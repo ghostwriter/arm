e74173a87fa35123d62e54a0d30349e9141c5772
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:24 -0500

    Add UpdateComposerPackageVersion.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Task/UpdateComposerPackageVersion.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Task;
+
+use Ghostwriter\Arm\Configuration\Process;
+use Ghostwriter\Arm\Interface\Task\Composer\ComposerPackageTaskInterface;
+use Override;
+
+use function sprintf;
+
+final readonly class UpdateComposerPackageVersion implements ComposerPackageTaskInterface
+{
+    public function __construct(
+        private array $packages
+    ) {
+    }
+
+    #[Override]
+    public function __invoke(string $organization, string $repository): void
+    {
+        $path = sprintf('%s/%s', $organization, $repository);
+
+        foreach ($this->packages as $package => $version) {
+            $this->update($package, $version, $path);
+        }
+    }
+
+    #[Override]
+    public function packages(): array
+    {
+        return $this->packages;
+    }
+
+    private function update(string $package, string $path, string $version): void
+    {
+        (new Process(['composer', 'require', sprintf('%s:%s', $package, $version)], $path))->mustRun();
+    }
+}
