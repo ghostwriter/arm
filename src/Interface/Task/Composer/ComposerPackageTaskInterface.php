72ab6da8ded70eaf40d6720983c5b3e7201778f5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add ComposerPackageTaskInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Task/Composer/ComposerPackageTaskInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\Task\Composer;
+
+use Ghostwriter\Arm\Interface\Task\ComposerTaskInterface;
+
+interface ComposerPackageTaskInterface extends ComposerTaskInterface
+{
+}
