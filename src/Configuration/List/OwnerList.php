be0ace36035b5eb08355575d114f611b701018cf
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add OwnerList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Configuration/List/OwnerList.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Configuration\List;
+
+use Ghostwriter\Arm\Interface\ArrayableInterface;
+use Ghostwriter\Arm\Interface\GitHub\OwnerInterface;
+use Override;
+
+final class OwnerList implements ArrayableInterface
+{
+    /**
+     * @param array<class-string<OwnerInterface>,class-string<OwnerInterface>> $owners
+     */
+    public function __construct(
+        private array $owners = [],
+    ) {
+    }
+
+    /**
+     * @param class-string<OwnerInterface> ...$owners
+     *
+     * @return $this
+     */
+    public function add(string ...$owners): self
+    {
+        foreach ($owners as $owner) {
+            $this->owners[$owner] = $owner;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array<class-string<OwnerInterface>,class-string<OwnerInterface>>
+     */
+    #[Override]
+    public function toArray(): array
+    {
+        return $this->owners;
+    }
+
+    public static function new(): self
+    {
+        return new self();
+    }
+}
