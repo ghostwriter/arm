03453ab9725e4a289d56ab77dbcec8001dbee08d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:24 -0500

    Add NameInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Composer/NameInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\Composer;
+
+use Ghostwriter\Arm\Interface\StringableInterface;
+
+interface NameInterface extends StringableInterface
+{
+}
