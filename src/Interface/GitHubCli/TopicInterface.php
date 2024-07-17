0efbe40aeb21029055424412a15441c68d9f9078
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add TopicInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHubCli/TopicInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHubCli;
+
+use Ghostwriter\Arm\Interface\GitHubCliInterface;
+use Ghostwriter\Arm\Interface\StringableInterface;
+
+interface TopicInterface extends GitHubCliInterface, StringableInterface
+{
+}
