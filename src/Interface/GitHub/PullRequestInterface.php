1a94ffa91f288323161b6164e855ac0d1acfde26
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add PullRequestInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHub/PullRequestInterface.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHub;
+
+use Ghostwriter\Arm\Interface\GitHub\Branch\BaseBranchInterface;
+use Ghostwriter\Arm\Interface\GitHub\Branch\HeadBranchInterface;
+use Ghostwriter\Arm\Interface\GitHub\PullRequest\BodyInterface;
+use Ghostwriter\Arm\Interface\GitHub\PullRequest\TitleInterface;
+use Ghostwriter\Arm\Interface\RepositoryInterface;
+
+interface PullRequestInterface
+{
+    public function baseBranch(): BaseBranchInterface;
+
+    public function body(): BodyInterface;
+
+    public function create(string $pullRequest): string;
+
+    public function create(string $pullRequest): string;
+
+    public function create(): void;
+
+    public function delete(string $pullRequest): string;
+
+    public function delete(string $pullRequest): string;
+
+    public function delete(): void;
+
+    public function exists(string $pullRequest): bool;
+
+    public function exists(string $pullRequest): bool;
+
+    public function get(string $pullRequest): string;
+
+    public function get(string $pullRequest): string;
+
+    public function headBranch(): HeadBranchInterface;
+
+    public function labels(): array;
+
+    public function list(): array;
+
+    public function list(): array;
+
+    public function owner(): OwnerInterface;
+
+    public function repository(): RepositoryInterface;
+
+    public function set(string $pullRequest): string;
+
+    public function set(string $pullRequest): string;
+
+    public function title(): TitleInterface;
+
+    public function update(TitleInterface $title, BodyInterface $body, array $labels = []): void;
+
+    /**
+     * @param LabelInterface $labels
+     */
+    public static function new(
+        OwnerInterface $owner,
+        RepositoryInterface $interfaceRepository,
+        BaseBranchInterface $baseBranch,
+        HeadBranchInterface $headBranch,
+        TitleInterface $title,
+        BodyInterface $body,
+        array $labels = []
+    ): void;
+}
