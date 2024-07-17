c69df8c29a46c11df851ff5067337a4759f30d70
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add WorkspaceInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/WorkspaceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface;
+
+interface WorkspaceInterface extends StringableInterface
+{
+    //    public function create(string $workspace): string;
+    //
+    //    public function delete(string $workspace): string;
+    //
+    //    public function exists(string $workspace): bool;
+    //
+    //    public function get(string $workspace): string;
+    //
+    //    public function list(): array;
+    //
+    //    public function set(string $workspace): string;
+    public static function new(string $workspace): self;
+}
