3124aa3f13be9cacb1b5d49354835c666b674c13
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add ProjectInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/ProjectInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface;
+
+interface ProjectInterface
+{
+    public function create(string $project): string;
+
+    public function delete(string $project): string;
+
+    public function exists(string $project): bool;
+
+    public function get(string $project): string;
+
+    public function list(): array;
+
+    public function set(string $project): string;
+}
