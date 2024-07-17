aa2c2e265bbd0f4ad6be5374216b1eb90de7d03d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add GitTaskInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Task/GitTaskInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\Task;
+
+use Ghostwriter\Arm\Interface\TaskInterface;
+
+interface GitTaskInterface extends TaskInterface
+{
+    //    public function branch(): string;
+    //
+    //    public function commit(): string;
+    //
+    //    public function tag(): string;
+}
