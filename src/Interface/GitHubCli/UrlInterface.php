d5b9a414ce9a8c419709aa60af3108f9ed43cafb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add UrlInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHubCli/UrlInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHubCli;
+
+use Ghostwriter\Arm\Interface\GitHubCliInterface;
+use Ghostwriter\Arm\Interface\StringableInterface;
+
+interface UrlInterface extends GitHubCliInterface, StringableInterface
+{
+}
