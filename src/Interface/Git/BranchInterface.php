e4f14152c491d4d357b0d0dabf485230a5c7c273
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add BranchInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Git/BranchInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\Git;
+
+use Ghostwriter\Arm\Interface\StringableInterface;
+use Ghostwriter\Arm\Tool\GitHubCli\RepositoryInterface;
+
+interface BranchInterface extends StringableInterface
+{
+    public function checkout(string $branch): string;
+
+    public function create(string $branch): string;
+
+    public function delete(string $branch): string;
+
+    public function exists(string $branch): bool;
+
+    public function get(string $branch): string;
+
+    public function list(): array;
+
+    public function repository(): RepositoryInterface;
+
+    public function set(string $branch): string;
+
+    public static function new(string $branch): self;
+}
