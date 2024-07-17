379213fa6ddf4f9a07696414d48b968203bf28fb
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add RepositoryList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Configuration/List/RepositoryList.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Configuration\List;
+
+use Ghostwriter\Arm\Interface\ArrayableInterface;
+use Ghostwriter\Arm\Interface\RepositoryInterface;
+use Override;
+
+final class RepositoryList implements ArrayableInterface
+{
+    /**
+     * @param array<class-string<RepositoryInterface>,class-string<RepositoryInterface>> $repositories
+     */
+    public function __construct(
+        private array $repositories = [],
+    ) {
+    }
+
+    /**
+     * @param class-string<RepositoryInterface> ...$repositories
+     *
+     * @return $this
+     */
+    public function add(string ...$repositories): self
+    {
+        foreach ($repositories as $repository) {
+            $this->repositories[$repository] = $repository;
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return array<class-string<RepositoryInterface>,class-string<RepositoryInterface>>
+     */
+    #[Override]
+    public function toArray(): array
+    {
+        return $this->repositories;
+    }
+
+    public static function new(): self
+    {
+        return new self();
+    }
+}
