f811d378c4bcacd6e7c5c2cb0998b4099b528ad9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add BaseBranchInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHub/Branch/BaseBranchInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHub\Branch;
+
+use Ghostwriter\Arm\Interface\GitHub\BranchInterface;
+
+interface BaseBranchInterface extends BranchInterface
+{
+}
