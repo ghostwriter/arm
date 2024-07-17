b11edb000f64551e2193f03c22a15b5e31cd7dda
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add CommandRunner.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Configuration/CommandRunner.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Configuration;
+
+use Ghostwriter\Shell\Interface\ShellInterface;
+use RuntimeException;
+
+use function array_shift;
+
+final readonly class CommandRunner
+{
+    public function __construct(
+        private ShellInterface $shell,
+        private array $repositories
+    ) {
+    }
+
+    public function run(): void
+    {
+        foreach ($this->repositories as $repository => $arguments) {
+            $command = array_shift($arguments);
+
+            $result = $this->shell->execute($command, $arguments, $repository);
+
+            if ($result->exitCode() !== 0) {
+                throw new RuntimeException($result->stderr());
+            }
+        }
+    }
+}
