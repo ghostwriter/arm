bfa920d478edaa4efee8a5677ac400c1a6e516e0
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add TaskList.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Configuration/List/TaskList.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Configuration\List;
+
+use Ghostwriter\Arm\Interface\ArrayableInterface;
+use Ghostwriter\Arm\Interface\TaskInterface;
+use Override;
+
+use function array_merge;
+
+final class TaskList implements ArrayableInterface
+{
+    /**
+     * @param array<class-string<TaskInterface>,array<string,array<string,string>|string>> $tasks
+     */
+    public function __construct(
+        private array $tasks = [],
+    ) {
+    }
+
+    /**
+     * @param array<class-string<TaskInterface>,array<string,array<string,string>|string>> $tasks
+     *
+     * @return $this
+     */
+    public function add(array $tasks): self
+    {
+        $this->tasks = array_merge($this->tasks, $tasks);
+
+        return $this;
+    }
+
+    /**
+     * @return array<class-string<TaskInterface>,array<string,array<string,string>|string>>
+     */
+    #[Override]
+    public function toArray(): array
+    {
+        return $this->tasks;
+    }
+
+    public static function new(): self
+    {
+        return new self();
+    }
+}
