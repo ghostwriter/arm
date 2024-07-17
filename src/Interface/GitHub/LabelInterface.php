18eed87fb4a3413e35da2205d36f8484944e2e87
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:23 -0500

    Add LabelInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/GitHub/LabelInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\GitHub;
+
+use Ghostwriter\Arm\Interface\StringableInterface;
+
+interface LabelInterface extends StringableInterface
+{
+}
