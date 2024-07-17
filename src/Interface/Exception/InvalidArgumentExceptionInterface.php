6f01030a85e5c8aed0ea581df9a626e6020dfa9d
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:24 -0500

    Add InvalidArgumentExceptionInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/Exception/InvalidArgumentExceptionInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface\Exception;
+
+use Ghostwriter\Arm\Interface\ExceptionInterface;
+
+interface InvalidArgumentExceptionInterface extends ExceptionInterface
+{
+}
