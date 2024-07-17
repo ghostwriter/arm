7a62fb94544facc02b50b1e19b86646201c4585a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:22 -0500

    Add RepositoryInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Interface/RepositoryInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Interface;
+
+interface RepositoryInterface
+{
+    public function clone(string $repository): string;
+
+    public function create(string $repository): string;
+
+    public function delete(string $repository): string;
+
+    public function exists(string $repository): bool;
+
+    public function get(string $repository): string;
+
+    public function list(): array;
+
+    public function set(string $repository): string;
+}
