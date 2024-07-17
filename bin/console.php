55ffc7024dc5b15959b5b9ffdd67b1c9d609affe
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:24 -0500

    Update console.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- bin/console.php
+++ bin/console.php
@@ -3,25 +3,23 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Wip\Console;
-
-use Ghostwriter\Wip\Foo;
+use Ghostwriter\Arm\Console\Application;
 
 use const STDERR;
 
-use function fwrite;
-use function sprintf;
-
 /** @var ?string $_composer_autoload_path */
 (static function (string $composerAutoloadPath): void {
-    /** @psalm-suppress UnresolvableInclude */
-    require $composerAutoloadPath ?: fwrite(
-        STDERR,
-        sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
-    ) && exit(1);
 
-    /**
-     * #BlackLivesMatter.
-     */
-    echo (new Foo())->test();
-})($_composer_autoload_path ?? null);
+    if (! \file_exists($composerAutoloadPath)) {
+        \fwrite(
+            STDERR,
+            \sprintf('[ERROR]Cannot locate "%s"\n please run "composer install"\n', $composerAutoloadPath)
+        );
+        exit(1);
+    }
+
+    require $composerAutoloadPath;
+
+    /** #BlackLivesMatter */
+    Application::new()->run();
+})($_composer_autoload_path ?? \dirname(__DIR__) . '/vendor/autoload.php');
