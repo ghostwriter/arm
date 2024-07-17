016230b8eab1a0ef70e08afbec5f052016da256a
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-07-17 13:59:24 -0500

    Add RemoveFile.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ src/Task/RemoveFile.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\Arm\Task;
+
+use Ghostwriter\Arm\Interface\TaskInterface;
+
+final readonly class RemoveFile implements TaskInterface
+{
+}
