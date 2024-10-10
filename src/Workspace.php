2bc281987e3eaefa744e9f55d66cf7309d7e1265
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-09 21:18:00 -0500

    Update Workspace.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Workspace.php
+++ src/Workspace.php
@@ -10,33 +10,30 @@ use Ghostwriter\Container\Attribute\Factory;
 use InvalidArgumentException;
 use Override;
 
-use function is_dir;
-use function trim;
-
 #[Factory(WorkspaceFactory::class)]
 final readonly class Workspace implements WorkspaceInterface
 {
     public function __construct(
         private string $workspace
     ) {
-        if (trim($workspace) === '') {
+        if (\trim($workspace) === '') {
             throw new InvalidArgumentException('Workspace cannot be empty');
         }
 
-        if (! is_dir($workspace)) {
+        if (! \is_dir($workspace)) {
             throw new InvalidArgumentException('Workspace must be a directory');
         }
-    }
-
-    #[Override]
-    public function toString(): string
-    {
-        return $this->workspace;
     }
 
     #[Override]
     public static function new(string $workspace): self
     {
         return new self($workspace);
+    }
+
+    #[Override]
+    public function toString(): string
+    {
+        return $this->workspace;
     }
 }
