054ae0e07fe34a92dd9bce93c6866681204a24d7
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-09 21:16:30 -0500

    Update Application.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -12,9 +12,6 @@ use Ghostwriter\Container\Container;
 use const PHP_EOL;
 use const PHP_VERSION;
 
-use function sprintf;
-use function var_dump;
-
 final readonly class Application
 {
     public function __construct(
@@ -25,10 +22,21 @@ final readonly class Application
     ) {
     }
 
+    public static function new(): self
+    {
+        $container = Container::getInstance();
+
+        if (! $container->has(ServiceProvider::class)) {
+            $container->provide(ServiceProvider::class);
+        }
+
+        return $container->get(self::class);
+    }
+
     public function run(): void
     {
         $container = Container::getInstance();
-        echo sprintf('%s %s by %s and contributors.', $this->name, $this->version, $this->author), PHP_EOL, PHP_EOL;
+        echo \sprintf('%s %s by %s and contributors.', $this->name, $this->version, $this->author), PHP_EOL, PHP_EOL;
 
         echo 'Runtime: PHP ', PHP_VERSION, PHP_EOL;
 
@@ -42,10 +50,10 @@ final readonly class Application
             ->toArray();
         echo 'Repositories:', PHP_EOL;
         foreach ($repositories as $repository=> $taskListw) {
-            var_dump($repository, $taskListw);
+            \var_dump($repository, $taskListw);
             echo 'Tasks:', PHP_EOL;
             foreach ($taskList as $class => $task) {
-                var_dump($class, $task);
+                \var_dump($class, $task);
 
                 $container->invoke($class, [$container->build(Repository::class, [$repository]), $task]);
             }
@@ -60,16 +68,5 @@ final readonly class Application
             //                echo $topic->toString(), PHP_EOL;
             //            }
         }
-    }
-
-    public static function new(): self
-    {
-        $container = Container::getInstance();
-
-        if (! $container->has(ServiceProvider::class)) {
-            $container->provide(ServiceProvider::class);
-        }
-
-        return $container->get(self::class);
     }
 }
