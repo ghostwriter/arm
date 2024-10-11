5b85801d345ec5df250767cb3bc6e9e92958cd5b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 15:37:09 -0500

    Update OrganizationInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/GitHub/Owner/OrganizationInterface.php
+++ src/Interface/GitHub/Owner/OrganizationInterface.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Arm\Interface\GitHub\Owner;
+namespace Ghostwriter\AutomatedRepositoryManager\Interface\GitHub\Owner;
 
-use Ghostwriter\Arm\Interface\GitHub\OwnerInterface;
+use Ghostwriter\AutomatedRepositoryManager\Interface\GitHub\OwnerInterface;
 
 interface OrganizationInterface extends OwnerInterface
 {
