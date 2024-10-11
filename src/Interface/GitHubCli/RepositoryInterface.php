66682d8a89a36989cb7b4dc36744d62cb31a59ec
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 15:37:09 -0500

    Update RepositoryInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/GitHubCli/RepositoryInterface.php
+++ src/Interface/GitHubCli/RepositoryInterface.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Arm\Interface\GitHubCli;
+namespace Ghostwriter\AutomatedRepositoryManager\Interface\GitHubCli;
 
-use Ghostwriter\Arm\Interface\GitHubCliInterface;
-use Ghostwriter\Arm\Interface\StringableInterface;
+use Ghostwriter\AutomatedRepositoryManager\Interface\GitHubCliInterface;
+use Ghostwriter\AutomatedRepositoryManager\Interface\StringableInterface;
 
 interface RepositoryInterface extends GitHubCliInterface, StringableInterface
 {
