43a9a9ca1234e9dc6d4d7ae8bbcea6b23b674db9
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 15:37:11 -0500

    Update TaskProcessorTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- tests/Unit/TaskProcessorTest.php
+++ tests/Unit/TaskProcessorTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Tests\Unit;
 
 use Generator;
-use Ghostwriter\Arm\TaskProcessor;
+use Ghostwriter\AutomatedRepositoryManager\TaskProcessor;
 use Ghostwriter\Container\Container;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\DataProvider;
