1b05cf0bfd42f8c3f2c726537fb612ef8d1e6899
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 15:37:08 -0500

    Update MessageInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Git/Commit/MessageInterface.php
+++ src/Interface/Git/Commit/MessageInterface.php
@@ -2,9 +2,9 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Arm\Interface\Git\Commit;
+namespace Ghostwriter\AutomatedRepositoryManager\Interface\Git\Commit;
 
-use Ghostwriter\Arm\Interface\StringableInterface;
+use Ghostwriter\AutomatedRepositoryManager\Interface\StringableInterface;
 
 interface MessageInterface extends StringableInterface
 {
