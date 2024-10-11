926217a69b67e03c1b4b72eb6eb0938b13efdf0c
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 15:37:08 -0500

    Update BranchInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/GitHub/BranchInterface.php
+++ src/Interface/GitHub/BranchInterface.php
@@ -2,10 +2,10 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Arm\Interface\GitHub;
+namespace Ghostwriter\AutomatedRepositoryManager\Interface\GitHub;
 
-use Ghostwriter\Arm\Interface\GitHubCliInterface;
-use Ghostwriter\Arm\Interface\StringableInterface;
+use Ghostwriter\AutomatedRepositoryManager\Interface\GitHubCliInterface;
+use Ghostwriter\AutomatedRepositoryManager\Interface\StringableInterface;
 
 interface BranchInterface extends GitHubCliInterface, StringableInterface
 {
