e5a170411c5da177700b988955f05ba76679b2ef
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-10-11 15:37:07 -0500

    Update VersionConstraintInterface.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/Interface/Composer/VersionConstraintInterface.php
+++ src/Interface/Composer/VersionConstraintInterface.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Ghostwriter\Arm\Interface\Composer;
+namespace Ghostwriter\AutomatedRepositoryManager\Interface\Composer;
 
 interface VersionConstraintInterface
 {
